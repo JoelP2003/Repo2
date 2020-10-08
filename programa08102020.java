b157045cd7d72c2d8ba52dd60b0d97b17c344e57
Unknown <unknown@example.com>
2020-10-08 07:13:47 -0500

    archivo

---
+++ programa08102020.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bi_claseslinea;
+
+/**
+ *
+ * @author JoelPR
+ */
+public class programa08102020 {
+    public static void main(String[] args) {
+        System.out.println("tags");
+    }
+       
+}
